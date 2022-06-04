6df0fc7715dc33874fc377b49220723b035045a1
AlexTeklu <alexanderosteklu@gmail.com>
2022-06-04 21:29:59 +0200

    updated login

--- server/router/Auth.js
+++ server/router/Auth.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import Users from '../models/User.js';
 import CryptoJS from 'crypto-js';
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
 const authRouter = express.Router();
+
+///////////////////registering user///////////////
 
 async function registerUser(req, res) {
 	try {
@@ -30,67 +32,70 @@ async function registerUser(req, res) {
 
 ///login
 
-// async function userLogin(req, res) {
-//     try {
-//         const user = await Users.findOne({
-//             username: req.body.username,
-//         });
-
-//         if (user) {
-//             const hashedPassword = CryptoJS.AES.decrypt(
-//                 user.password,
-//                 process.env.PASS_SEC
-//             );
-//             const { password, ...others } = user._doc;
-//             const definedPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-//             definedPassword === req.body.password;
-
-//             if (definedPassword === req.body.password) {
-//                 return res.status(200).json({ ...others });
-//             } else {
-//                 return res.status(401).json('Incorrect password');
-//             }
-//         } else {
-//             return res.status(401).json('User not found');
-//         }
-//     } catch (error) {
-//         res.status(500).json(error);
-//     }
-// }
-
 async function userLogin(req, res) {
 	try {
 		const user = await Users.findOne({
 			username: req.body.username,
+			
 		});
-		!user && res.status(401).json('Wrong credentials');
+		
 
-		const hashedPassword = CryptoJS.AES.decrypt(
-			user.password,
-			process.env.PASS_SEC
-		);
+		if (user) {
+			const hashedPassword = CryptoJS.AES.decrypt(
+				user.password,
+				process.env.PASS_SEC
+			);
+			const { password, ...others } = user._doc;
+			const definedPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+			definedPassword === req.body.password;
 
-		const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-		OriginalPassword !== req.body.password &&
-			res.status(401).json('wrong Credentials');
-
-		const accessToken = jwt.sign(
-			{
-				id: user._id,
-				isAdmin: user.isAdmin,
-			},
-			process.env.JWT_SEC,
-			{ expiresIn: '3d' }
-		);
-
-		const { password, ...others } = user._doc;
-
-		res.status(200).json({ ...others, accessToken });
-	} catch (e) {
-		res.status(500).json(e);
+			if (definedPassword === req.body.password) {
+				return res.status(200).json({ ...others });
+			} else {
+				return res.status(401).json('Incorrect password');
+			}
+		} else {
+			return res.status(401).json('User not found');
+		}
+	} catch (error) {
+		res.status(500).json(error);
 	}
-};
+}
+
+// async function userLogin(req, res) {
+// 	try {
+// 		const user = await Users.findOne({
+// 			 username: req.body.username,
+
+// 		});
+// 		 !user && res.status(401).json('Wrong credentials');
+
+// 		const hashedPassword = CryptoJS.AES.decrypt(
+// 			user.password,
+// 			process.env.PASS_SEC
+// 		);
+
+// 		const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+// 		OriginalPassword !== req.body.password &&
+// 	 	res.status(401).json('wrong Credentials');
+
+// const accessToken = jwt.sign(
+// 	{
+// 		id: user._id,
+// 		isAdmin: user.isAdmin,
+// 	},
+// 	process.env.JWT_SEC,
+// 	{ expiresIn: '3d' }
+// );
+
+// const { password, ...others } = user._doc;
+
+// res.status(200).json({ ...others, accessToken });
+// 	} catch (e) {
+// 		res.status(500).json(e);
+// 	}
+// }
 
 authRouter.post('/', registerUser);
- authRouter.post('/login', userLogin);
+authRouter.post('/login', userLogin);
 export default authRouter;
